121c34d4624912b6c5b4fc93737d9d39dbcc4633
unknown <bormeyprak@gmail.con>
2026-06-17 15:21:37 +0700

    initial commit

---
+++ src/main/java/com/example/idcard_management/IdcardManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.idcard_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdcardManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdcardManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/idcard_management/IdcardManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.idcard_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdcardManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
